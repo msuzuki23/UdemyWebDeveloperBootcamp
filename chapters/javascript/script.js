adb191afcad66750f402e2a6a1cee9569173393a
Markus Suzuki <markus_suzuki@hotmail.com>
2019-01-20 11:04:52 -0500

    Introduction to Javascript

---
+++ chapters/javascript/script.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your First Name?");
+var lastName = prompt("What is your Last Name?");
+var age = prompt("What is your Age?");
+alert("Hi " + firstName + " " + lastName + ". You are " + age + " years old.");
+console.log("Hi " + firstName + " " + lastName + ". You are " + age + " years old.");
