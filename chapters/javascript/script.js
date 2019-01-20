4ec414640d834bd7de18ce453afccbbb8bebfd56
Markus Suzuki <markus_suzuki@hotmail.com>
2019-01-20 16:57:29 -0500

    Javascript: Conditionals and Loops

--- chapters/javascript/script.js
+++ chapters/javascript/script.js
@@ -1,5 +1,55 @@
-var firstName = prompt("What is your First Name?");
-var lastName = prompt("What is your Last Name?");
-var age = prompt("What is your Age?");
-alert("Hi " + firstName + " " + lastName + ". You are " + age + " years old.");
-console.log("Hi " + firstName + " " + lastName + ". You are " + age + " years old.");
+// var firstName = prompt("What is your First Name?");
+// var lastName = prompt("What is your Last Name?");
+// var age = prompt("What is your Age?");
+// alert("Hi " + firstName + " " + lastName + ". You are " + age + " years old.");
+// console.log("Hi " + firstName + " " + lastName + ". You are " + age + " years old.");
+
+
+// var age = prompt("What is your age?")
+// if(age < 18) {
+//     console.log("Cannot go in");
+// }
+// else if(age < 21) {
+//     console.log("Cannot drink");
+// }
+// else {
+//     console.log("Can drink, have fun!");
+//}
+
+
+// var count = 1;
+// while(count < 6) {
+//     console.log("count is: " + count);
+//     count++;
+// }
+
+
+// var word = "Hello is that me you are looking for?...";
+// var count = 0;
+// while(count < word.length) {
+//     console.log(word[count]);
+//     count++;
+// }
+
+
+// var num = 300
+// while(num <= 333){
+//     if(num % 2 != 0){
+//         console.log(num);
+//     }
+//     num++;
+// }
+
+
+// var num = 5;
+// while(num <= 50){
+//     if((num % 3 === 0) && (num % 5 === 0)) {
+//         console.log(num);
+//     }
+//     num++;
+// }
+
+
+for(var count = 0; count <=5; count++){
+    console.log(count);
+}
