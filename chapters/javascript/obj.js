51c3c98f1803736b5683870fb0d69d27754c931f
Markus Suzuki <markus_suzuki@hotmail.com>
2019-01-22 04:55:44 -0500

    Javascript Methods

---
+++ chapters/javascript/obj.js
@@ -0,0 +1,20 @@
+// var person = {
+//     name: "Markus",
+//     age: 42,
+//     city: "Alpharetta",
+// }
+
+// var person2 = {
+//     name: "Liss",
+//     age: 41,
+//     city: "Alpharetta"
+// }
+
+// console.log(person.city);
+// console.log(person);
+// var arr = [person, person2];
+// console.log(arr);
+// console.log(arr[1].name);
+
+
+
